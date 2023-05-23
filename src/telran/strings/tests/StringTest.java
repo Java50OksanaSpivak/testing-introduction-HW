17aceee882d34d115204357e3b22566effefdca3
bazilda <bazilda@gmail.com>
2023-05-23 23:39:50 +0300

    HW #1

---
+++ src/telran/strings/tests/StringTest.java
@@ -0,0 +1,16 @@
+package telran.strings.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.Object.java;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest extends java {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
